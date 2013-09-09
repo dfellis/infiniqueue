1d2d0c84eeeb2d88e93f5a33fe8df7e277309111
David Ellis <d.f.ellis@ieee.org>
2013-09-09 11:20:52 -0700

    Forgot to write the browserify shim for generating the browser version, and fixing the prepublish script to fail if any sub-step fails.

--- prepublish.sh
+++ prepublish.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
+set -e
 
 npm test
+
 docco ./lib/infiniqueue.js
 git stash
-mv docs docs-new
 git checkout gh-pages
+mv docs/* .
+mv infiniqueue.html index.html
 rm -rf docs
-mv docs-new docs
+git add --all
 git commit -am "Automatic documentation for version $npm_package_version"
+
 git checkout master
 git stash pop
 browserify ./lib/browserify.js | uglifyjs > ./lib/queue-flow.min.js
 git commit -am "Automatic minification for version $npm_package_version"
+
 git tag $npm_package_version
 git push
 git push --tags
